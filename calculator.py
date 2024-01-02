d9479c7e1ef6f303e0d5b9d3d0165c4945cf3f5d
Simon <simcruchet@gmail.com>
2024-01-02 21:24:38 +0100

    design ok

--- calculator.py
+++ calculator.py
@@ -1,6 +1,31 @@
 import customtkinter
 
-customtkinter.set_appearance_mode("dark")
+def execute_operation():
+
+
+def execute_number_key(key):
+
+
+def execute_symbol_key(key):
+
+
+
+def pressed_key(key):
+    if key in ['0','1','2','3','4','5','6','7','8','9','.']:
+        execute_number_key(key)
+    elif key in ['+','-','*','÷','√','x²','=']:
+        execute_symbol_key(key)
+    elif key == 'AC':
+        execute_operation()
+
+button_size = 35
+button_typeface = 'Courrier'
+button_orange = '#ff9f0a'
+button_grey = '#a5a5a5'
+button_dark = '#333333'
+button_white = '#ffffff'
+button_black = '#000000'
+font_button = (button_typeface, button_size, 'bold')
 
 app = customtkinter.CTk()
 app.title('Calculator Made By Simon Cruchet')
@@ -8,36 +33,30 @@ app.geometry("380x600")
 app.configure(bg = '#ffffff')
 screen_font = ('Courrier', 55, 'bold')
 screen = customtkinter.CTkLabel(app, text="0", font = screen_font)
-screen.grid(row=0, column=0, sticky="E", padx=(10,25), pady=(20,30), columnspan=4)
+screen.grid(row=0, column=0, sticky="E", padx=(10,28), pady=(20,30), columnspan=4)
 
-button_size = 35
-button_typeface = 'Courrier'
-button_orange = '#ff9f0a'
-button_grey = '#a5a5a5'
-button_dark = '#333333'
-button_white = '#000000'
-button_black = '#ffffff'
-font_button = ('Courrier', 35, 'bold')
 
-zero = customtkinter.CTkButton(app, text="0", width=170, height=75, font = font_button, anchor='w', bg =button_dark ,fg =button_white)
-one = customtkinter.CTkButton(app, text="1", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-two = customtkinter.CTkButton(app, text="2", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-three = customtkinter.CTkButton(app, text="3", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-four = customtkinter.CTkButton(app, text="4", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-five = customtkinter.CTkButton(app, text="5", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-six = customtkinter.CTkButton(app, text="6", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-seven = customtkinter.CTkButton(app, text="7", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-eight = customtkinter.CTkButton(app, text="8", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-nine = customtkinter.CTkButton(app, text="9", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-plus = customtkinter.CTkButton(app, text="+", width=75, height=75, font = font_button, bg = button_orange ,fg = button_white)
-minus = customtkinter.CTkButton(app, text="-", width=75, height=75, font = font_button, bg = button_orange ,fg = button_white)
-divided = customtkinter.CTkButton(app, text="÷", width=75, height=75, font = font_button, bg = button_orange ,fg = button_white)
-times = customtkinter.CTkButton(app, text="*", width=75, height=75, font = font_button, bg = button_orange ,fg = button_white)
-sqrt = customtkinter.CTkButton(app, text="√", width=75, height=75, font = font_button, bg = button_grey,fg = button_black)
-power = customtkinter.CTkButton(app, text="x²", width=75, height=75, font = font_button, bg = button_grey,fg = button_black)
-egal = customtkinter.CTkButton(app, text="=", width=75, height=75,font = font_button, bg = button_orange ,fg = button_white)
-point = customtkinter.CTkButton(app, text=".", width=75, height=75, font = font_button, bg =button_dark ,fg =button_white)
-ac = customtkinter.CTkButton(app, text="AC", width=75, height=75, font = font_button, bg = button_grey,fg = button_black)
+
+
+zero = customtkinter.CTkButton(app, text="0", width=170, height=75, font = font_button, anchor='w', fg_color =button_dark ,text_color =button_white)
+one = customtkinter.CTkButton(app, text="1", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+two = customtkinter.CTkButton(app, text="2", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+three = customtkinter.CTkButton(app, text="3", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+four = customtkinter.CTkButton(app, text="4", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+five = customtkinter.CTkButton(app, text="5", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+six = customtkinter.CTkButton(app, text="6", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+seven = customtkinter.CTkButton(app, text="7", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+eight = customtkinter.CTkButton(app, text="8", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+nine = customtkinter.CTkButton(app, text="9", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+plus = customtkinter.CTkButton(app, text="+", width=75, height=75, font = font_button, fg_color = button_orange ,text_color = button_white)
+minus = customtkinter.CTkButton(app, text="-", width=75, height=75, font = font_button, fg_color = button_orange ,text_color = button_white)
+divided = customtkinter.CTkButton(app, text="÷", width=75, height=75, font = font_button, fg_color = button_orange ,text_color = button_white)
+times = customtkinter.CTkButton(app, text="*", width=75, height=75, font = font_button, fg_color = button_orange ,text_color = button_white)
+sqrt = customtkinter.CTkButton(app, text="√", width=75, height=75, font = font_button, fg_color = button_grey,text_color = button_black)
+power = customtkinter.CTkButton(app, text="x²", width=75, height=75, font = font_button, fg_color = button_grey,text_color = button_black)
+egal = customtkinter.CTkButton(app, text="=", width=75, height=75,font = font_button, fg_color = button_orange ,text_color = button_white)
+point = customtkinter.CTkButton(app, text=".", width=75, height=75, font = font_button, fg_color =button_dark ,text_color =button_white)
+ac = customtkinter.CTkButton(app, text="AC", width=75, height=75, font = font_button, fg_color = button_grey,text_color = button_black)
 
 zero.grid(row = 5, column = 0, pady=(10,10),   columnspan=2)
 one.grid(row = 4, column = 0, padx=(10,10), pady=(10,10))
